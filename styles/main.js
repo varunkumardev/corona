29c39cfe4b759d840fb26517193e649bcdb2400a
Varun Kumar Gunuru <varunkumarg8886@gmail.com>
2021-06-02 23:26:50 +0530

    uploaded corona live tracker

---
+++ styles/main.js
@@ -0,0 +1,26 @@
+$(document).ready(function () {
+    $('.demo').hide();
+
+    $(".cir1").click(function () {
+        $(".cir1").toggleClass("fa-plus-circle fa-minus-circle");
+        $('.demo').toggle();
+    });
+
+
+    $(".cir2").click(function () {
+        $(".cir2").toggleClass("fa-plus-circle fa-minus-circle");
+        $('.demo1').toggle();
+    });
+
+
+    $(".cir3").click(function () {
+        $(".cir3").toggleClass("fa-plus-circle fa-minus-circle");
+        $('.demo2').toggle();
+    });
+
+
+    $(".cir4").click(function () {
+        $(".cir4").toggleClass("fa-plus-circle fa-minus-circle");
+        $('.demo3').toggle();
+    });
+});
